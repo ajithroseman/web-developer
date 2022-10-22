3e1b53d5594d7eae10760383d6d24cac840ca3d3
ajithroseman <95347389+ajithroseman@users.noreply.github.com>
2022-10-22 10:35:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let menutoggle = document.querySelector('.menutoggle');
+let menu = document.querySelector('.menu');
+menutoggle.onclick = function(){
+    menu.classList.toggle('active')
+}
+ var a1 = 5
+ var a2 = 6
+ console.log(a1+a2)
